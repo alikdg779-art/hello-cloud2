7292f8e09691b13674365708e79b2d3a3a0fd423
Ali <alikdg779@gmail.com>
2025-10-29 12:04:30 +0300

    Fix typos and formatting in app.py
    Corrected various typos and formatting issues in HTML and Python code.

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_tempate_string, request
+from flask import Flask, render_template_string, request
 import os
 import psycopg2
 
 app = Flask(__name__)
 
 #Render'ın otomotik tnaımaldığı veritabanı bağlantı bilsigi(DATABASE_URL ortam değişkeni)
-DATABASE_URL = os. getenv("DATABASE_URL", "postgresql://ali:WcDzbUa45u5HayZCYjh0gXB30fVwnqox@dpg-d3tjfevdiees73djureg-a.oregon-postgres.render.com/hello_cloud2_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://ali:WcDzbUa45u5HayZCYjh0gXB30fVwnqox@dpg-d3tjfevdiees73djureg-a.oregon-postgres.render.com/hello_cloud2_db")
 
 # HTML ŞABLONU (tek sayfada form + liste)
 HTML = """
 <!doctype html>
 <html>
-<html>
 <head>
-  <title>Buluttan selam!</titlie>
+  <title>Buluttan selam!</title>
   <style>
-    bod { font-family: Arial: text-align: center ; padding: 50px; background #eef2f3; }
+    body { font-family: Arial; text-align: center ; padding: 50px; background: #eef2f3; }
     h1 { color: #333; }
     form { margin: 20px auto; }
     input { padding: 10px; font-size: 16px }
@@ -25,15 +24,15 @@ HTML = """
   </head>
   <body>
       <h1> Buluttan Selam!</h1>
-      <p> Adını yaz, selamaını bırak</>
+      <p> Adını yaz, selamaını bırak</p>
       <form method="POST">
         <input type="text" name="isim" placeholder="Adını yaz" required>
-        <button> type="submit">Gönder</button>
+        <button type="submit">Gönder</button>
       </form>
       <h3>Ziyaretçiler:</h3>
       <ul> 
-          {% for ad in isimle %}
-            <li<{{ ad }}</li>
+          {% for ad in isimler %}
+            <li>{{ ad }}</li>
           {% endfor %}
       </ul>
     </body>
@@ -41,16 +40,16 @@ HTML = """
     """
 
   def connect_db():
-      conn = psycopg2.connecr(DATABASE_URL)
+      conn = psycopg2.connect(DATABASE_URL)
       return conn
 
-  @app.route("/", methods=(["GET", "POST"])
+  @app.route("/", methods=["GET", "POST"])
   def index():
       conn = connect_db()
       cur = conn.cursor()
-      cur.execute("CREATE TABLE IF NOT EXISTS   ziyaretciler (id SERİAL PRIMARY KEY, isim TEXT)")
+      cur.execute("CREATE TABLE IF NOT EXISTS  ziyaretciler (id SERİAL PRIMARY KEY, isim TEXT)")
 
-      if request.method == "POST"
+      if request.method == "POST":
         isim = request.formget("isim")
       if isim:
         cur.execute("INSERT INTO ziyaretciler(isim) VALUES (%s)", (isim,))
@@ -63,7 +62,7 @@ cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
   conn.close()
   return render_template_string(HTML, isimler=isimler)
 
-if__name__ =="__main__":
+if__name__ == "__main__":
   app.run(host = "0.0.0.0" , port=5000)
   
 
